a45ba572328ab58e6234bdb36be5627980690f12
Jackson Sieben <jackson_sieben@live.com>
2023-05-06 19:14:19 -0300

    first commit - API done

---
+++ SistemaBiblioteca.API/Models/LivroModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaBiblioteca.API.Models
+{
+    public class LivroModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LivroID { get; set; }
+
+        [Required(ErrorMessage = "Autor é obrigatório!")]
+        public string? Autor { get; set; }
+
+        [Required(ErrorMessage = "ISBN é obrigatório!")]
+        public string? ISBN { get; set; }
+
+        [Required(ErrorMessage = "Ano de Publicação é obrigatória!")]
+        public int AnoPublicacao { get; set; }
+
+        [Required(ErrorMessage = "Quantidade disponível é obrigatória!")]
+        public int QuantidadeDisponivel { get; set; } 
+    }
+}
