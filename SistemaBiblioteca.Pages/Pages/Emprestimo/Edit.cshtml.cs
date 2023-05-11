7504f836c5601de3466a8e5770f6316b6c5cb4f2
Jackson Sieben <jackson_sieben@live.com>
2023-05-11 15:55:29 -0300

    Adjusts in emprestimoPage

--- SistemaBiblioteca.Pages/Pages/Emprestimo/Edit.cshtml.cs
+++ SistemaBiblioteca.Pages/Pages/Emprestimo/Edit.cshtml.cs
@@ -10,8 +10,9 @@ namespace SistemaBiblioteca.Pages.Pages.Emprestimo
     {
         [BindProperty]
         public EmprestimoModel EmprestimoModel { get; set; } = new();
-        public Edit(){
-        }
+        public List<BibliotecarioModel> BibliotecarioList { get; set; } = new();
+        public List<LivroModel> LivroList { get; set; } = new();
+        public Edit(){}
 
         public async Task<IActionResult> OnGetAsync(int? id){
             if(id == null){
@@ -29,7 +30,22 @@ namespace SistemaBiblioteca.Pages.Pages.Emprestimo
 
             var content = await response.Content.ReadAsStringAsync();
             EmprestimoModel = JsonConvert.DeserializeObject<EmprestimoModel>(content)!;
+
+            var httpClientBibliotecario = new HttpClient();
+            var urlBibliotecario = "http://localhost:5185/Bibliotecario";
+            var requestMessageBibliotecario = new HttpRequestMessage(HttpMethod.Get, urlBibliotecario);
+            var responseBibliotecario = await httpClientBibliotecario.SendAsync(requestMessageBibliotecario);
+            var contentBibliotecario = await responseBibliotecario.Content.ReadAsStringAsync();
+
+            BibliotecarioList = JsonConvert.DeserializeObject<List<BibliotecarioModel>>(contentBibliotecario)!;
             
+            var httpClientLivro = new HttpClient();
+            var urlLivro = "http://localhost:5185/Livro";
+            var requestMessageLivro = new HttpRequestMessage(HttpMethod.Get, urlLivro);
+            var responseLivro = await httpClientLivro.SendAsync(requestMessageLivro);
+            var contentLivro = await responseLivro.Content.ReadAsStringAsync();
+
+            LivroList = JsonConvert.DeserializeObject<List<LivroModel>>(contentLivro)!;
             return Page();
         }
 
@@ -40,10 +56,10 @@ namespace SistemaBiblioteca.Pages.Pages.Emprestimo
 
             var httpClient = new HttpClient();
             var url = $"http://localhost:5185/Emprestimo/Edit/{id}";
-            var bibliotecarioJson = JsonConvert.SerializeObject(EmprestimoModel);
+            var emprestimoJson = JsonConvert.SerializeObject(EmprestimoModel);
 
             var requestMessage = new HttpRequestMessage(HttpMethod.Put, url);
-            requestMessage.Content = new StringContent(bibliotecarioJson, Encoding.UTF8, "application/json");
+            requestMessage.Content = new StringContent(emprestimoJson, Encoding.UTF8, "application/json");
 
             var response = await httpClient.SendAsync(requestMessage);
 
